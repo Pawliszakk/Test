0ba05036ba0cb68d92b60b06039591276a97a0e5
Pawliszakk <oskar.pawliszak1@o2.pl>
2023-02-22 10:42:12 +0100

    added a site

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const burgerBtn = document.querySelector('.burger-btn');
+const burgerBtnBars = document.querySelector('.burger-btn__bars');
 const navMenu = document.querySelector('.nav');
 const navItems = document.querySelectorAll('.nav__items');
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year');
 
 const navOnOff = () => {
     navMenu.classList.toggle('active');
+    burgerBtnBars.classList.remove('black-burger-bars')
     navItemsAnimation();
 };
 
@@ -16,6 +20,33 @@ const navItemsAnimation = () => {
     });
 };
 
+
+const currentYear = () =>{
+    const year = (new Date).getFullYear();
+    footerYear.textContent = year;
+}
+
+
+const burgerSpy = () => {
+        const currentSection = window.scrollY;
+
+        allSections.forEach(section => {
+
+            if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+                burgerBtnBars.classList.add('black-burger-bars')
+            }else if(!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+                burgerBtnBars.classList.remove('black-burger-bars')
+            }
+
+        })
+}
+
+
+
+
+currentYear();
 burgerBtn.addEventListener('click', navOnOff);
 
 navItems.forEach((item) => item.addEventListener('click', navOnOff));
+
+window.addEventListener('scroll', burgerSpy);
